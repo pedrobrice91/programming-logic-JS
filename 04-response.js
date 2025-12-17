cca13b242165a04eb19418bf94b2764f81b7ab7a
Pedro L Briceno <pedrobrice91@gmail.com>
2025-12-17 09:16:35 -0300

    añadi archivo html

--- 04-response.js
+++ 04-response.js
@@ -1,11 +1,36 @@
-function AdivinaElNumero(num){
-    const numeroSecreto = (Math.floor(Math.random()))*1
-    if (num <= 17){
-        console.log(numeroSecreto)
-    }else{
-        console.log('No es un numero valido')
+const numeroSecreto = Math.floor(Math.random() * 19);
+
+let num = parseInt(prompt("Dame un numero❓❓"));
+
+function AdivinaElNumero(num) {
+  let intentos = 0;
+
+  if (num != numeroSecreto) {
+    if (num <= 17) {
+      if (numeroSecreto >= num) {
+        alert(
+          "🤨 Mal. El numero secreto es mayor al numero ingresado. La respuesta es " +
+            numeroSecreto
+        );
+        intentos += 1;
+        //vuelve a preguntar
+      } else {
+        alert(
+          "🙃 Noo. El numero secreto es menor al numero ingresado. La respuesta es " +
+            numeroSecreto
+        );
+        intentos += 1;
+        //vuelve a preguntar
+      }
+    } else {
+      alert("No es un numero valido");
+      /// vuelve a preguntar
     }
+  } else {
+    alert("Adivinaste 🥳🥳 ❤️" + ".Numero de intentos " + intentos);
+  }
 }
 
-AdivinaElNumero(10)
-AdivinaElNumero(17)
+AdivinaElNumero(num);
+//AdivinaElNumero(17)
+//AdivinaElNumero(18)
